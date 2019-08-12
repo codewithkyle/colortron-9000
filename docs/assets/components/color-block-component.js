826243ede592fd6434b53efc5a298651bc5d4d4b
Kyle Andrews <kylea@page.works>
2019-08-12 18:51:00 -0400

    Adds initial color block component

---
+++ docs/assets/components/color-block-component.js
@@ -0,0 +1,25 @@
+class ColorBlockComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.getStylesheet();
+    }
+    getStylesheet() {
+        let stylesheet = document.head.querySelector('[component="color-block-component"]');
+        if (!stylesheet) {
+            stylesheet = document.createElement('style');
+            stylesheet.setAttribute('component', 'color-block-component');
+            document.head.appendChild(stylesheet);
+            fetch(`${window.location.origin}${window.location.pathname}assets/styles/color-block-component.css`)
+                .then(request => request.text())
+                .then(response => {
+                stylesheet.innerHTML = response;
+            })
+                .catch(error => {
+                console.error(error);
+            });
+        }
+    }
+    connectedCallback() {
+    }
+}
+customElements.define('color-block-component', ColorBlockComponent);
