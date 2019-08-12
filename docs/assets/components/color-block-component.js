7aca22845a18581b25744bef2583f2a307b4447f
Kyle Andrews <kylea@page.works>
2019-08-12 19:01:52 -0400

    Adds event listeners to color blocks

--- docs/assets/components/color-block-component.js
+++ docs/assets/components/color-block-component.js
@@ -1,7 +1,13 @@
 class ColorBlockComponent extends HTMLElement {
     constructor() {
         super();
+        this.handleDeleteClick = this.removeColor.bind(this);
+        this.handleEditClick = this.editColor.bind(this);
+        this.handleColorClick = this.activateColor.bind(this);
         this.getStylesheet();
+        this._deleteButton = this.querySelector('.js-delete-button');
+        this._editButton = this.querySelector('.js-edit-button');
+        this._colorButton = this.querySelector('.js-color-button');
     }
     getStylesheet() {
         let stylesheet = document.head.querySelector('[component="color-block-component"]');
@@ -19,7 +25,22 @@ class ColorBlockComponent extends HTMLElement {
             });
         }
     }
+    removeColor() {
+        this.remove();
+    }
+    editColor() {
+    }
+    activateColor() {
+    }
     connectedCallback() {
+        this._deleteButton.addEventListener('click', this.handleDeleteClick);
+        this._editButton.addEventListener('click', this.handleEditClick);
+        this._colorButton.addEventListener('click', this.handleColorClick);
+    }
+    disconnectedCallback() {
+        this._deleteButton.removeEventListener('click', this.handleDeleteClick);
+        this._editButton.removeEventListener('click', this.handleEditClick);
+        this._colorButton.removeEventListener('click', this.handleColorClick);
     }
 }
 customElements.define('color-block-component', ColorBlockComponent);
--- src/color-block-component.ts
+++ src/color-block-component.ts
@@ -1,9 +1,17 @@
 class ColorBlockComponent extends HTMLElement
 {
+    private _deleteButton : HTMLButtonElement;
+    private _editButton : HTMLButtonElement;
+    private _colorButton : HTMLElement;
+
     constructor()
     {
         super();
         this.getStylesheet();
+
+        this._deleteButton = this.querySelector('.js-delete-button');
+        this._editButton = this.querySelector('.js-edit-button');
+        this._colorButton = this.querySelector('.js-color-button');
     }
 
     private getStylesheet()
@@ -25,9 +33,37 @@ class ColorBlockComponent extends HTMLElement
         }
     }
 
-    connectedCallback()
+    private handleDeleteClick:EventListener = this.removeColor.bind(this);
+    private handleEditClick:EventListener = this.editColor.bind(this);
+    private handleColorClick:EventListener = this.activateColor.bind(this);
+
+    private removeColor() : void
+    {
+        this.remove();
+    }
+
+    private editColor() : void
     {
 
+    }
+
+    private activateColor() : void
+    {
+        
+    }
+
+    connectedCallback()
+    {
+        this._deleteButton.addEventListener('click', this.handleDeleteClick);
+        this._editButton.addEventListener('click', this.handleEditClick);
+        this._colorButton.addEventListener('click', this.handleColorClick);
+    }
+
+    disconnectedCallback()
+    {
+        this._deleteButton.removeEventListener('click', this.handleDeleteClick);
+        this._editButton.removeEventListener('click', this.handleEditClick);
+        this._colorButton.removeEventListener('click', this.handleColorClick);
     }
 }
 
