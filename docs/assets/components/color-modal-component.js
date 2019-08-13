41b4dac13fb201d6482d48cecf2a910814493375
Kyle Andrews <kylea@page.works>
2019-08-13 08:29:27 -0400

    Adds modal popup animation/transition

--- docs/assets/components/color-modal-component.js
+++ docs/assets/components/color-modal-component.js
@@ -3,7 +3,13 @@ class ColorModalComponent extends HTMLElement {
         super();
         this.handleBackdropClick = this.closeModal.bind(this);
         this.handleKeypress = this.manageKeys.bind(this);
+        this.handleFormSubmit = this.addColor.bind(this);
         this._backdrop = this.querySelector('color-modal-backdrop');
+        this._form = this.querySelector('form');
+        this._closeButton = this.querySelector('button[type="close"]');
+    }
+    addColor(e) {
+        e.preventDefault();
     }
     closeModal() {
         this.remove();
@@ -16,10 +22,14 @@ class ColorModalComponent extends HTMLElement {
     connectedCallback() {
         this._backdrop.addEventListener('click', this.handleBackdropClick);
         document.body.addEventListener('keyup', this.handleKeypress);
+        this._form.addEventListener('submit', this.handleFormSubmit);
+        this._closeButton.addEventListener('click', this.handleBackdropClick);
     }
     disconnectedCallback() {
         document.body.removeEventListener('keyup', this.handleKeypress);
         this._backdrop.removeEventListener('click', this.handleBackdropClick);
+        this._form.removeEventListener('submit', this.handleFormSubmit);
+        this._closeButton.removeEventListener('click', this.handleBackdropClick);
     }
 }
 customElements.define('color-modal-component', ColorModalComponent);
--- src/color-modal-component.ts
+++ src/color-modal-component.ts
@@ -2,15 +2,25 @@ class ColorModalComponent extends HTMLElement
 {
 
     private _backdrop : HTMLElement;
+    private _form : HTMLFormElement;
+    private _closeButton : HTMLButtonElement;
 
     constructor()
     {
         super();
         this._backdrop = this.querySelector('color-modal-backdrop');
+        this._form = this.querySelector('form');
+        this._closeButton = this.querySelector('button[type="close"]');
     }
 
     private handleBackdropClick:EventListener = this.closeModal.bind(this);
     private handleKeypress:EventListener = this.manageKeys.bind(this);
+    private handleFormSubmit:EventListener = this.addColor.bind(this);
+
+    private addColor(e:Event) : void
+    {
+        e.preventDefault();
+    }
 
     private closeModal() : void
     {
@@ -29,12 +39,16 @@ class ColorModalComponent extends HTMLElement
     {
         this._backdrop.addEventListener('click', this.handleBackdropClick);
         document.body.addEventListener('keyup', this.handleKeypress);
+        this._form.addEventListener('submit', this.handleFormSubmit);
+        this._closeButton.addEventListener('click', this.handleBackdropClick);
     }
 
     disconnectedCallback()
     {
         document.body.removeEventListener('keyup', this.handleKeypress);
         this._backdrop.removeEventListener('click', this.handleBackdropClick);
+        this._form.removeEventListener('submit', this.handleFormSubmit);
+        this._closeButton.removeEventListener('click', this.handleBackdropClick);
     }
 }
 
